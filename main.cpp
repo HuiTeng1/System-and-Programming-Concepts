685768fa7af6315c394376b04639b2688942414d
MervynMiao <mervynlim0509@gmail.com>
2025-08-12 12:47:17 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    
+    return 0;
+}
+
+void monitoring(){
+    /*1. add the visitor 
+    -> Please enter the visitor name: 
+    -> show comfirmation message, if not loop back 
+    -> 
+    */ 
+}
